3a409120b693dcc907cc6250d20114fdbb44bc2a
snowmaltea <snowmaltea@gmail.com>
2025-10-03 19:49:30 +0900

    MAGame 생성

---
+++ MAGame/Source/MAGame/MAGame.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MAGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MAGame, "MAGame" );
+ 
---
+++ MAGame/Source/MAGame/MAGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MAGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMAGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMAGameGameMode();
+};
+
+
+
